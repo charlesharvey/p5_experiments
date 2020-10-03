30bcd80436e256619f7e1a15e6b0ee77fad33530
Charles Harvey <harvey.charles@gmail.com>
2020-10-03 12:16:04 +0100

    add koch snowflake

---
+++ kochsnowflake/part.js
@@ -0,0 +1,47 @@
+class Part {
+
+    constructor(x1, y1, x2, y2) {
+        this.a = createVector(x1, y1);
+        this.b = createVector(x2, y2);
+
+        this.c = p5.Vector.lerp(this.a, this.b, 0.3333);
+        this.d = p5.Vector.lerp(this.a, this.b, 0.6667);
+        this.e = p5.Vector.lerp(this.a, this.b, 0.5);
+
+
+    }
+
+    splitInThree() {
+
+
+
+
+        const r = p5.Vector.dist(this.a, this.b);
+
+
+        const f = p5.Vector.sub(this.b, this.a);
+        f.rotate(PI / 2);
+        f.setMag(r / pointyness);
+        f.add(this.e);
+
+        const n1 = new Part(this.a.x, this.a.y, this.c.x, this.c.y);
+        const n2 = new Part(this.d.x, this.d.y, this.b.x, this.b.y);
+        const n3 = new Part(this.c.x, this.c.y, f.x, f.y);
+        const n4 = new Part(f.x, f.y, this.d.x, this.d.y);
+        return [n1, n2, n3, n4];
+
+    }
+
+
+
+    show() {
+
+
+        stroke(255, 150);
+        strokeWeight(1);
+        noFill()
+        line(this.a.x, this.a.y, this.b.x, this.b.y);
+
+
+    }
+}
---
+++ kochsnowflake/scripts.js
@@ -0,0 +1,109 @@
+
+
+
+let maxw;
+let parts;
+let colortheta = 0;
+const grid = 3;
+let pointyness;
+function setup() {
+
+
+    createCanvas(windowWidth - 12, windowHeight - 12);
+    colorMode(HSB);
+
+    maxw = min(width / 2.5, height / 2.5);
+
+
+
+    reset();
+
+
+}
+
+function reset() {
+    pointyness = random(0.7, 5);
+    parts = [];
+    for (let i = 0; i < 3; i++) {
+        const w = maxw;
+        const theta1 = TWO_PI / 3 * i;
+        const theta2 = TWO_PI / 3 * (i + 1);
+        const x2 = sin(theta2) * w;
+        const y2 = cos(theta2) * w;
+        const x1 = sin(theta1) * w;
+        const y1 = cos(theta1) * w;
+
+        const cx = width / 2;
+        const cy = height / 2;
+        const p = new Part(x1 + cx, y1 + cy, x2 + cx, y2 + cy);
+        parts.push(p);
+
+    };
+}
+
+function draw() {
+    background(0);
+
+    // translate(width / 2, height / 2);
+
+
+
+    // loadPixels();
+
+    // updatePixels();
+
+    // for (let x = 0; x < width; x += grid) {
+    //     for (let y = 0; y < height; y += grid) {
+
+
+    //         record_d = 100000;
+    //         parts.forEach(part => {
+    //             d = dist(part.e.x, part.e.y, x, y);
+    //             if (d < record_d) {
+    //                 record_d = d;
+    //             }
+    //         })
+
+    //         fill((record_d * 3 + colortheta) % 255, 100, 100);
+    //         noStroke();
+    //         rect(x, y, grid, grid);
+
+    //     }
+    // }
+
+
+
+    parts.forEach(part => {
+        part.show();
+    });
+
+
+
+    if (frameCount % 30 == 0) {
+        splitParts();
+    }
+
+
+
+}
+function splitParts() {
+    loop();
+    if (parts.length > 5000) {
+        reset();
+    } else {
+        newparts = [];
+        parts.forEach(part => {
+            const np = part.splitInThree();
+            np.map(np => newparts.push(np));
+        });
+
+        parts = newparts;
+
+    }
+
+}
+
+function keyPressed() {
+    splitParts();
+
+}
