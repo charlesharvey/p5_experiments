3fd9b8f2a33c6b2aa771c63c7d4b2339804d9320
Charles Harvey <harvey.charles@gmail.com>
2020-10-13 19:13:07 +0100

    add cellular automata

---
+++ cellularautomata/scripts.js
@@ -0,0 +1,135 @@
+
+let grid;
+let rows, cols;
+let generations;
+let rules;
+let ruleNumber;
+let h;
+
+function setup() {
+
+    createCanvas(windowWidth - 20, windowHeight - 20);
+    colorMode(HSB);
+    // grid = 5;
+    // rows = Math.ceil(height / grid);
+    // cols = Math.ceil(width / grid);
+
+    cols = 150;
+    grid = Math.ceil(width / cols);
+    rows = Math.ceil(height / grid / 2);
+    reset();
+
+}
+
+
+function reset() {
+    ruleNumber = Math.floor(random(0, 255));
+    rules = makeRuleSetFromNumber(ruleNumber);
+    h = 0;
+    generations = [];
+
+
+    let generation = makeGeneration();
+    generation[Math.floor(cols / 2)] = 1;
+    // if (Math.random() > 0.5) {
+    //     generation = makeRandomGeneration();
+    // }
+
+
+    generations.push(generation);
+
+
+}
+
+
+function makeRuleSetFromNumber(n) {
+    n = Math.floor(n);
+    let r = [0, 0, 0, 0, 0, 0, 0, 0];
+    const bins = [128, 64, 32, 16, 8, 4, 2, 1];
+    bins.forEach((b, bi) => {
+        if (n >= b) {
+            r[bi] = 1;
+            n = n - b;
+        }
+    });
+    return r;
+}
+
+
+function mousePressed() {
+    reset();
+}
+
+function makeGeneration() {
+    const g = [];
+    for (let i = 0; i < cols; i++) {
+        g[i] = 0;
+    }
+    return g;
+}
+function makeRandomGeneration() {
+    const g = [];
+    for (let i = 0; i < cols; i++) {
+        g[i] = Math.round(Math.random());
+    }
+    return g;
+}
+
+
+
+function applyRule(a, b, c) {
+    const ind = a * 4 + b * 2 + c;
+
+    // console.log(a, b, c);
+    return rules[ind];
+}
+
+
+
+function draw() {
+    background(0);
+
+    noStroke();
+
+    h += 0.005;
+
+    generations.forEach((generation, i) => {
+        const y = i * grid;
+        generation.forEach((cell, j) => {
+            const x = j * grid;
+
+            h += 0.00007;
+
+            fill(h % 255, 255, cell * 255);
+            rect(x, y, grid, grid);
+        })
+    });
+
+
+
+
+    let new_generation = makeGeneration();
+    for (let i = 1; i < generations[0].length - 1; i++) {
+        const a = generations[0][i - 1]
+        const b = generations[0][i + 0]
+        const c = generations[0][i + 1];
+        const newval = applyRule(a, b, c);
+        new_generation[i] = newval;
+    }
+
+
+    generations.unshift(new_generation)
+
+    if (generations.length > rows) {
+        generations.pop();
+    }
+
+    // noLoop();
+
+    // frameRate(30);
+
+    fill(100, 0, 200);
+    text(`Rule Number: ${ruleNumber}`, 20, height - 40);
+    text(`Click to change rule`, 20, height - 20);
+
+}
