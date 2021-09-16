8d38e5870c4bb534f372fd645fe50103dd49e778
Charles Harvey <harvey.charles@gmail.com>
2021-09-16 11:37:38 +0100

    quadratic spline

--- dotsonspline/spline.js
+++ dotsonspline/spline.js
@@ -8,23 +8,27 @@ class Spline {
 
 
         const p1x = map(theta, 0, 1, 100, 200);
-        const p1y = 100;
+        const p1y = height / 2;
 
         const p2x = map(theta, 0, 1, width - 100, width - 200);
-        const p2y = 100;
+        const p2y = height / 2;
 
-        const c1x = map(theta, 0, 1, width / 2 - 100, width / 2 + 100);
-        const c1y = height;
+        const c1x = map(theta, 0, 1, width / 2 - 400, width / 2 - 300);
+        const c1y = map(theta, 0, 1, -50, -150)
+
+        const c2x = map(theta, 0, 1, width / 2 + 300, width / 2 + 400);
+        const c2y = map(theta, 0, 1, height + 500, height + 650)
 
 
         this.point1 = createVector(p1x, p1y);
         this.point2 = createVector(p2x, p2y);
         this.control1 = createVector(c1x, c1y);
+        this.control2 = createVector(c2x, c2y);
         this.dotsize = 3;
         this.numberofdots = 50;
-        // this.speed = map(theta, 0, 1, 0.001, 0.006);
-        // this.speed = random(0.001, 0.006);
-        this.speed = 0.003;
+        // this.speed = map(theta, 0, 1, 0.001, 0.004);
+        this.speed = random(0.001, 0.004);
+        // this.speed = 0.003;
 
 
     }
@@ -44,7 +48,17 @@ class Spline {
             const p = (this.percentage + (i / this.numberofdots)) % 1;
             let d1 = p5.Vector.lerp(this.point1, this.control1, p);
             let d2 = p5.Vector.lerp(this.control1, this.point2, p);
-            let dot = p5.Vector.lerp(d1, d2, p);
+
+
+            let p1 = p5.Vector.lerp(d1, d2, p);
+            let d3 = p5.Vector.lerp(this.point1, this.control2, p);
+            let d4 = p5.Vector.lerp(this.control2, this.point2, p);
+            let p2 = p5.Vector.lerp(d3, d4, p);
+            let g1 = p5.Vector.lerp(p1, p2, p);
+
+
+            let dot = g1; //p1;
+
             const v = p - 0.5;
             const w = 1 / Math.exp(v * v);
             const b = map(w, 0.8, 1, 0, 255);
