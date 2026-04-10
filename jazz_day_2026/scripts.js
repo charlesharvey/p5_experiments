266ca8081b47d9be5306e254efe55bf8ce844bb2
Charles Harvey <harvey.charles@gmail.com>
2026-04-10 16:51:18 +0100

    asdf

--- jazz_day_2026/scripts.js
+++ jazz_day_2026/scripts.js
@@ -2,6 +2,9 @@ let font;
 
 let lights = [];
 
+let shapes = [];
+let mouse;
+
 class Light {
   constructor(x, w) {
     this.x1 = x;
@@ -33,6 +36,45 @@ class Light {
   }
 }
 
+class Shaaape {
+  constructor(points) {
+    this.points = points;
+    this.bottom_points = this.bottom();
+  }
+
+  bottom() {
+    const ys = this.points.map((p) => p.y);
+    const maxy = max(ys);
+    return this.points.filter((p) => p.y == maxy);
+  }
+
+  show() {
+    noStroke();
+    fill(0);
+
+    beginShape();
+    this.points.forEach((p) => {
+      vertex(p.x, p.y);
+    });
+    endShape();
+  }
+
+  shadow() {
+    if (mouse) {
+      fill(117, 117, 55);
+      beginShape();
+      vertex(mouse.x, height);
+
+      this.bottom_points.forEach((p) => {
+        vertex(p.x, p.y);
+      });
+      // vertex(x1, y2);
+      // vertex(x2, y2);
+      endShape();
+    }
+  }
+}
+
 function preload() {
   // font = loadFont("/agenda.ttf");
 }
@@ -41,17 +83,52 @@ function setup() {
   createCanvas(windowWidth - 20, windowHeight - 20);
   // console.log(font);
 
-  for (let i = 0; i < 7; i++) {
-    const x = random(0, width);
-    const w = random(20, 300);
-    const l = new Light(x, w);
-    lights.push(l);
+  // for (let i = 0; i < 7; i++) {
+  //   const x = random(0, width);
+  //   const w = random(20, 300);
+  //   const l = new Light(x, w);
+  //   lights.push(l);
+  // }
+
+  shapes = [];
+
+  const dots = 11;
+  for (let i = 0; i < dots; i++) {
+    const y = i % 2 == 0 ? 100 : 200;
+    const x = map(i, 0, dots, 0, width);
+    const pts = createSquare(x, y, (width / dots) * 0.8);
+    shapes.push(new Shaaape(pts));
+  }
+
+  mouse = createVector(0, 0);
+}
+
+function createSquare(x, y, w) {
+  const a = createVector(x, y);
+  const b = createVector(x + w, y);
+  const c = createVector(x + w, y + w);
+  const d = createVector(x, y + w);
+
+  return [a, b, c, d];
+}
+
+function mouseMoved() {
+  if (mouse) {
+    mouse.x = mouseX;
+    mouse.y = mouseY;
   }
 }
 
 function draw() {
-  background(0);
-  lights.forEach((l) => {
-    l.show();
+  background(200, 200, 0);
+  // lights.forEach((l) => {
+  //   l.show();
+  // });
+
+  shapes.forEach((shp) => {
+    shp.shadow();
+  });
+  shapes.forEach((shp) => {
+    shp.show();
   });
 }
